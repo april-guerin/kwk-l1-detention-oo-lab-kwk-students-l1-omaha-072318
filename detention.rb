ba55e59fefbb23ae734985a43ed6360a3088039c
april-guerin <aprilguerin@yahoo.com>
2018-07-27 20:19:07 +0000

    automatically backed up by learn

--- detention.rb
+++ detention.rb
@@ -1,1 +1,41 @@
 #Code your detention class here
+class Detention
+  def initialize()
+    @activity = "collective punishment"
+  end
+  def activity
+    @activity = "collective punishment"
+  end
+  
+  def location=(location)
+    @location = location
+  end
+  def location
+    @location
+  end
+    
+  def time=(time)
+    @time = time
+  end
+  def time
+    @time
+  end
+  
+  def grumpy_teacher=(grumpy_teacher)
+    @grumpy_teacher = grumpy_teacher
+  end
+  def grumpy_teacher
+    @grumpy_teacher
+  end
+  
+  def students
+    @students = []
+  end
+  
+  def add_student=(student)
+    @student = @student << student
+  end
+  def add_student
+    @student
+  end
+end
